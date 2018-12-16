5f31e75cdb80e4e923cd03d91245ac1143a6d29a
Vera <a-scarlett@mail.ru>
2018-12-16 10:23:15 +0300

    hw11

---
+++ hw11.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include<vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{const int INF = 100;
+     setlocale(LC_ALL, "Russian");
+cout <<"Количество вершин "<<endl;
+int n;
+cin>> n;
+vector < vector < pair<int,int> > > g (n);
+pair<int, int> a;
+vector < pair<int,int> > b;
+int w[n];
+for (int i=0; i<n; i++){w[i]=0;}
+
+for (int i=0; i<n; i++){int k, x;
+cout <<"Вершина "<< i+1 <<" связана с  "<< endl;
+cin>> k;
+while (k!=0){cout <<"Веc ребра равен "<< endl;
+cin>> x;
+a.first=k-1;
+a.second=x;
+b.push_back(a);
+w[i]++;
+cout <<"Вершина "<< i+1 <<" связана с  "<< endl;
+cin>>k;}
+g[i]=b;
+b.clear();}
+for (int i=0; i<n; i++){
+    for (int j=0;j<w[i]; j++)
+{cout << i+1 <<" :  ";
+cout<< g[i][j].first+1 <<" (" << g[i][j].second<< ")"<<endl;}}
+
+for (int s=0; s<n; s++){
+    cout<< "from "<< s+1<< " to "<< endl;
+vector<int> d (n, INF),  p (n);
+	d[s] = 0;
+	vector <char> u (n);
+	for (int i=0; i<n; ++i) {
+		int v = -1;
+		for (int j=0; j<n; ++j)
+			if (!u[j] && (v == -1 || d[j] < d[v]))
+				v = j;
+		if (d[v] == INF)
+			break;
+		u[v] = true;
+
+		for (size_t j=0; j<g[v].size(); ++j) {
+			int to = g[v][j].first,
+				len = g[v][j].second;
+			if (d[v] + len < d[to]) {
+				d[to] = d[v] + len;
+				p[to] = v;}}}
+for (int t=0; t<n; t++){
+        cout<< t+1<< " - "<< d[t] << endl;}
+
+d.clear();
+p.clear();
+u.clear();
+}}
